b4be1dfa0824e517bd760be078f6750b0710a218
Lakan Haraya <lakanharaya@gmail.com>
2024-11-27 11:01:40 +0800

    Dagdagan ng mga paunang liligpit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+// Mga karagdagang ingklusyon
+
+
+void setup() {
+  // Kodigo ng inisyalisasyon
+  Serial.begin(115200);
+  Serial.println("\n	~~~ KUMUSTA, MUNDO! ~~~\n");
+  // Magtakda ng iba pang inisyalisasyon
+  
+  
+  delay(500);
+}
+void loop() {
+  // Pangunahing lohika ng programa
+  Serial.println("Lumilikaw...");
+  
+  
+  
+  delay(1000);
+}
